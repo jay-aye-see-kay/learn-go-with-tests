7779335eba9f98335d90cee3026772efbb404735
Jack Rose <user@jackrose.co.nz>
2022-11-24 14:33:14 +1100

    setup nix direnv for correct go version

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A flake with go 1.18";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive pkgs.go_1_18 ];
+        buildInputs = [ ];
+      };
+    });
+}
